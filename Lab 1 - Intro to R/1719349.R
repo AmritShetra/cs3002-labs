9d62faa22ccd02ef022582a2df7a0ee5196b8c0e
Amrit Shetra <1719349@brunel.ac.uk>
2020-09-29 13:20:47 +0100

    Added lab 1 files.

---
+++ Lab 1 - Intro to R/1719349.R
@@ -0,0 +1,38 @@
+# Load in the CSV file, comma delimited
+mydata = read.csv('forestfires.csv', sep=",")
+
+# Plot the data
+plot(mydata)
+
+# Produce a table from the data
+View(mydata)
+
+# Scatterplots of individual columns ($colname)
+plot(mydata$temp, mydata$wind)
+plot(mydata[,9], mydata[,11])
+
+# Historgram of a column
+hist(mydata$temp)
+
+# Line plot of a column
+plot(mydata$temp, type="l")
+
+# Plot X and Y coordinates, colour coded according to temperature
+plot(mydata$X, mydata$Y, col=mydata$temp)
+
+# Calculate mean, median, max and min values for temp column
+meantemp = mean(mydata$temp)
+mediantemp = median(mydata$temp)
+maxtemp = max(mydata$temp)
+mintemp = min(mydata$temp)
+
+# Write variable to CSV
+write.csv(
+  data.frame(meantemp, mediantemp, maxtemp, mintemp),
+  file = "Output.csv"
+)
+
+# Build a linear model using regression
+plot(mydata$temp, mydata$ISI)
+lmfire=line(mydata$ISI~mydata$temp)
+abline(coef(lmfire))

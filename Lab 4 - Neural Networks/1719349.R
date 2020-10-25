de831316e981e197d56295f4780b9736b79efaed
Amrit Shetra <1719349@brunel.ac.uk>
2020-10-25 23:17:09 +0000

    Tested the NN on an input of (1, 1) which works.

--- Lab 4 - Neural Networks/1719349.R
+++ Lab 4 - Neural Networks/1719349.R
@@ -32,3 +32,16 @@ plot(NN)
 
 # Look at the random weights and biases
 print(NN$weights)
+
+# Let's use `compute` to see if the network responds to an input of (1, 1)
+test_in = rbind(c(1,1))
+predict_testNN = compute(NN, test_in)
+
+# The activation of the output neuron is here
+predict_testNN$net.result
+
+# To calculate the discrete class, we threshold it at 0.5
+predict_out = as.numeric(predict_testNN$net.result>0.5)
+
+# This should predict the output as 1
+print(predict_out)

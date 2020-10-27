ce54587f2290c8aa5147ad1d4fa05be4e26cb9c4
Amrit Shetra <1719349@brunel.ac.uk>
2020-10-27 15:57:57 +0000

    Cleaned up code for the first half of Lab 4, way easier to read what's going on.

--- Lab 4 - Neural Networks/1719349.R
+++ Lab 4 - Neural Networks/1719349.R
@@ -7,18 +7,6 @@
 # Does it learn to correctly classify all the inputs?
 # -----------------
 
-predict <- function(neural_network, test_data) {
-  predict_testNN = compute(neural_network, test_data)
-  print(predict_testNN$neurons)
-  return(predict_testNN$net.result)
-}
-
-classify_predictions <- function(net_result) {
-  # To calculate the discrete class, we threshold it at 0.5
-  return(as.numeric(net_result>0.5))
-}
-
-
 # Set up a training set - we'll go with an OR gate
 train_in <- rbind(
   c(1, 1),
@@ -30,31 +18,26 @@ train_out <- rbind(1, 1, 1, 0)
 
 # Fit the neural network (with no hidden layers)
 set.seed(2)
-NN <- neuralnet(
-  train_out~.,
-  train_in,
-  hidden=0,
-  threshold=0.001,
-  stepmax=1e+05,
-  linear.output=FALSE
-)
+NN <- neuralnet(train_out~., train_in, hidden=0, threshold=0.001,
+                stepmax=1e+05, linear.output=FALSE)
 
 # Visualise the NN
 plot(NN)
 
-# Look at the random weights and biases
-print(NN$weights)
-
 # Let's see how the network responds to an input of (1, 1)
 test_in <- rbind(c(1,1))
 
-# This should predict the output as 1
-net_result <- predict(NN, test_in)
-predict_out <- classify_predictions(net_result)
-print(predict_out)
+# Make predictions using a specified neural network
+result <- compute(NN, test_in)
+raw_predictions <- result$net.result
 
+# To calculate the discrete class, we threshold it at 0.5
+final_predictions <- as.numeric(raw_predictions>0.5)
 
-# Set up the test output (XOR gate)
+# This should predict the output as 1 - we now know the NN works
+print(final_predictions)
+
+# Set up the XOR test input
 test_in <- rbind(
   c(-1, -1),
   c(1, -1),
@@ -63,6 +46,9 @@ test_in <- rbind(
 )
 
 # This doesn't classify the XOR problem correctly as there is no hidden layer
-net_result <- predict(NN, test_in)
-predict_out <- classify_predictions(net_result)
-print(predict_out)
+result <- compute(NN, test_in)
+raw_predictions <- result$net.result
+# print(result$neurons)
+print(raw_predictions)
+final_predictions <- as.numeric(raw_predictions>0.5)
+print(final_predictions)
